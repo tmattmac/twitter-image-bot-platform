c714d9aedf883a70b766a1a34adae1147869b7c7
Trey <tmcdeane@gmail.com>
2021-03-18 20:41:53 -0400

    prettied up dialog and added Roboto font

--- client/src/components/ImageEditDialog.jsx
+++ client/src/components/ImageEditDialog.jsx
@@ -1,29 +1,54 @@
-import { Button, Dialog, DialogContent, makeStyles, TextField } from "@material-ui/core";
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, makeStyles, TextField, useMediaQuery, useTheme } from "@material-ui/core";
+import clsx from 'clsx';
 import { useCallback, useEffect, useState } from "react";
 
 const useStyles = makeStyles(theme => ({
-  root: {
-    
+  dialogWrapper: {
+    display: 'flex',
+    flexFlow: 'column nowrap'
   },
   image: {
     maxWidth: '100%',
-    maxHeight: '80vh'
+    height: 'auto',
+    minHeight: 100,
+    maxHeight: '60vh',
+    margin: '0 auto',
+    alignSelf: 'center'
+  },
+  dialogContent: {
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  form: {
+    width: '100%',
+    display: 'flex',
+    alignItems: 'center'
+  },
+  formInput: {
+    flexGrow: 2
   }
 }));
 
 const ImageEditDialog = ({ image, open, handleClose: handleCloseDialog }) => {
+  const [displayImage, setDisplayImage] = useState(image);
   const [caption, setCaption] = useState(image?.source || '');
   const [isSaved, setIsSaved] = useState(true);
   const classes = useStyles();
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const handleClose = useCallback(() => {
     if (isSaved) handleCloseDialog();
   }, [handleCloseDialog, isSaved]);
 
   useEffect(() => {
-    setCaption(image?.source || '');
-    setIsSaved(true);
-  }, [image])
+    if (image) {
+      setDisplayImage(image);
+      setCaption(image?.source || '');
+      setIsSaved(true);
+    }
+  }, [image]);
 
   const handleFormChange = (event) => {
     setCaption(event.target.value);
@@ -36,21 +61,39 @@ const ImageEditDialog = ({ image, open, handleClose: handleCloseDialog }) => {
   }
 
   return (
-    <Dialog open={open} onClose={handleClose} maxWidth="lg">
-      <DialogContent>
-        <img src={image?.url} alt={image?.id} className={classes.image} />
-        <form onSubmit={handleSubmit}>
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      scroll={fullScreen ? "paper" : "body"}
+      fullScreen={fullScreen}
+      maxWidth="lg"
+      fullWidth
+      PaperProps={{
+        className: clsx({ [classes.dialogWrapper]: fullScreen })
+      }}
+    >
+      <DialogTitle>Edit Image Caption</DialogTitle>
+      <DialogContent className={classes.dialogContent} dividers>
+        <img src={displayImage?.url} alt={displayImage?.id} className={classes.image} />
+      </DialogContent>
+      <DialogActions>
+        <form onSubmit={handleSubmit} className={classes.form}>
           <TextField
             label="Caption"
             placeholder="This message will be tweeted alongside the image"
             value={caption}
             onChange={handleFormChange}
+            className={classes.formInput}
+            variant="outlined"
           />
-          <Button disabled={isSaved}>
+          <Button disabled={isSaved} type="submit" color="primary">
             Save
           </Button>
+          <Button color="secondary" onClick={handleCloseDialog}>
+            Cancel
+          </Button>
         </form>
-      </DialogContent>
+      </DialogActions>
     </Dialog>
   )
 }
